d110133db548bdbd1e9059d51ca0f327040a433c
maxbbn <qipbbn@gmail.com>
2013-11-18 14:45:11 +0800

    Add test for build file existence

--- test/index.js
+++ test/index.js
@@ -1,18 +1,44 @@
 var expect = require('expect.js'),
     nodeKpc = require('..');
 
-describe('node-kpc', function() {
-  it('Run without errors', function(done) {
+var fs = require('fs');
 
-    nodeKpc.buildPackage({
-        'package': {
-            name: 'xcake',
-            path: 'sample/src',
-            ignorePackageNameInUri: true
-        },
-        dest: 'sample/build'
+describe('node-kpc', function () {
+    before(function(){
+        "use strict";
+        nodeKpc.buildPackage({
+            'package': {
+                name: 'xcake',
+                path: 'sample/src',
+                ignorePackageNameInUri: true
+            },
+            dest: 'sample/build'
+        });
     });
 
-    done();
-  });
+    it('build all files', function (done) {
+
+        expect(fs.existsSync('sample/build/deps.js')).to.be(true);
+
+        expect(fs.existsSync('sample/build/app/example.css')).to.be(true);
+        expect(fs.existsSync('sample/build/app/index.js')).to.be(true);
+        expect(fs.existsSync('sample/build/app/mod.js')).to.be(true);
+        expect(fs.existsSync('sample/build/app/namedMod.js')).to.be(true);
+
+        expect(fs.existsSync('sample/build/components/header/index.js')).to.be(true);
+        expect(fs.existsSync('sample/build/components/header/mod.js')).to.be(true);
+
+        expect(fs.existsSync('sample/build/components/package-config/index.js')).to.be(true);
+
+        expect(fs.existsSync('sample/build/components/slide/index.js')).to.be(true);
+        expect(fs.existsSync('sample/build/components/slide/mod.js')).to.be(true);
+
+        expect(fs.existsSync('sample/build/components/tooltip/index.js')).to.be(true);
+        expect(fs.existsSync('sample/build/components/tooltip/mod.js')).to.be(true);
+
+        expect(fs.existsSync('sample/build/pages/home/index.js')).to.be(true);
+        expect(fs.existsSync('sample/build/pages/home/mod.js')).to.be(true);
+        expect(fs.existsSync('sample/build/pages/home/page.css')).to.be(true);
+        done();
+    });
 });
